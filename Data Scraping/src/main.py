fee146bbb9803e2949ca62e875dca589a6068fd0
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2022-06-17 15:26:03 +0700

    feat: add result data as JSON

--- Data Scraping/src/main.py
+++ Data Scraping/src/main.py
@@ -2,14 +2,14 @@ import scraper
 
 while True:
     try:
-        start = int(input("Enter the start index (0-58): "))
-        end = int(input("Enter the end index (1-59): "))
+        start = int(input("Enter the start index (0-57): "))
+        end = int(input("Enter the end index (1-58): "))
 
         # input validation
         if (end <= start):
             print("Last index should greater than the start index!")
             continue
-        elif (start < 0 or start > 58 or end < 1 or end > 59):
+        elif (start < 0 or start > 57 or end < 1 or end > 58):
             print("Please enter a valid index!")
             continue
         else:
@@ -18,4 +18,4 @@ while True:
         print("Please enter integers only!")
         exit()
 
-scraper.startScrape(start, end) # start scraping from start to last index
+scraper.startScrape(start, end + 1) # start scraping from start to last index
