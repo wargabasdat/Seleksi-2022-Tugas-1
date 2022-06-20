6a24d0827d9628527715999e459f3d730f7d9287
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2022-06-20 19:37:56 +0700

    feat: connect player handler and router

--- Data Storing/api/internal/routes/player/player.go
+++ Data Storing/api/internal/routes/player/player.go
@@ -1,10 +1,14 @@
 package playerRoutes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	playerHandler "api/internal/handlers/player"
+
+)	
 
 func SetupPlayerRoutes(router fiber.Router) {
-	player := router.Group("player")
+	player := router.Group("/player")
 
-	player.Get("/", func(c *fiber.Ctx) error{}) // read all players
-	player.Get("/:playerId", func(c *fiber.Ctx) error{}) // read player by id
+	player.Get("/", playerHandler.GetPlayer) // read all players
+	player.Get("/:playerId", playerHandler.GetPlayer) // read player by id
 }
