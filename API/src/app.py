e45e0089ac53c2374d290fbf5190812eb43da1df
addinnabilal <16520342@std.stei.itb.ac.id>
2022-06-26 12:52:33 +0700

    add CRUD API

---
+++ API/src/app.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+
+from json import load
+import sys
+sys.path.append("../")
+
+from flask import Flask
+import os
+from models import db
+
+from config import app_config
+from flask_migrate import Migrate
+from views.companyView import company_api as company_blueprint
+
+# app initiliazation
+load_dotenv()
+env_name = os.getenv('FLASK_ENV')
+app = Flask(__name__)
+app.config.from_object(app_config[env_name])
+db.init_app(app)
+Migrate(app,db)
+
+app.register_blueprint(company_blueprint, url_prefix='/api/top_companies') 
+
+# run app
+if __name__ == "__main__":
+    app.run()
--- Data Scraping/src/app.py
+++ Data Scraping/src/app.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-import json
+import json, sys
+from psycopg2 import connect, Error
+from psycopg2.extras import Json
+import config
 
-def get_companies_data():
+def getCompaniesData():
     url = "https://www.ycombinator.com/topcompanies"
 
     page = requests.get(url)
@@ -46,9 +49,9 @@ def get_companies_data():
         company_job_portal = company_job_portal_element["href"] if company_job_portal_element else None
 
         data = {
+            "company_rank": company_rank,
             "company_name": company_name,
             "company_website": company_website,
-            "company_rank": company_rank,
             "company_url_logo": company_url_logo,
             "company_overview": company_overview,
             "company_status": company_status,
@@ -62,7 +65,113 @@ def get_companies_data():
 
     return results
 
+def storeCompaniesDataToJson():
+    results = getCompaniesData()
+    with open('../data/data.json', 'w', encoding='utf-8') as outfile:
+        json.dump(results, outfile, indent=2, ensure_ascii=False)
 
-results = get_companies_data()
-with open('../data/data.json', 'w', encoding='utf-8') as outfile:
-    json.dump(results, outfile, indent=2, ensure_ascii=False)
+# MANUALLY INSERT INTO POSTGRESQL (WITHOUT API)
+
+# def create_company_data_tables():
+#     sql_command = (
+#         """
+#         CREATE TABLE IF NOT EXISTS company (
+#         company_rank INTEGER PRIMARY KEY,
+#         company_name VARCHAR(35) NOT NULL,
+#         company_website VARCHAR(50) NOT NULL,
+#         company_url_logo VARCHAR(150),
+#         company_overview VARCHAR(200)  NOT NULL,
+#         company_status VARCHAR(6) NOT NULL,
+#         company_batch VARCHAR(5) NOT NULL,
+#         company_breakthrough boolean,
+#         company_headquarters VARCHAR(50) NOT NULL,
+#         company_job_portal   VARCHAR(150)
+#         )
+#         """
+#     )
+#     conn = None
+#     try:
+#         # connect to the PostgreSQL server
+#         conn = connect(
+#             dbname = "top_companies",
+#             user = "postgres",
+#             password = "addinaddin123",
+#             # attempt to connect for 3 seconds then raise exception
+#             connect_timeout = 3
+#         )
+#         cur = conn.cursor()
+#         # create table one by one
+#         cur.execute(sql_command)
+#         # close communication with the PostgreSQL database server
+#         cur.close()
+#         # commit the changes
+#         conn.commit()
+#     except (Exception, Error) as err:
+#         print ("\npsycopg2 connect error:", err)
+#     finally:
+#         if conn is not None:
+#             conn.close()
+
+# def storeJsonToDB():
+#     # create table company
+#     create_company_data_tables();
+
+#     with open('../../data/data.json', 'r', encoding='utf-8') as json_data:
+#         record_list = json.load(json_data)
+
+#     rows = [list(x.values()) for x in record_list]
+#     columns = [list(x.keys()) for x in record_list][0]
+
+#     # declare a new PostgreSQL connection object
+#     conn = None
+#     try:
+#         # connect to the PostgreSQL server
+#         conn = connect(
+#             dbname = "top_companies",
+#             user = "postgres",
+#             password = "addinaddin123",
+#             # attempt to connect for 3 seconds then raise exception
+#             connect_timeout = 3
+#         )
+#         cur = conn.cursor()
+#         values = ""
+#         for i, record in enumerate(rows):
+
+#             # declare empty list for values
+#             values_list = []
+        
+#             # append each value to a new list of values
+#             for v, val in enumerate(record):
+#                 if type(val) == str:
+#                     val = str(Json(val)).replace('"', '')
+#                 if (not(val)):
+#                     val = "NULL"
+#                 values_list += [ str(val) ]
+
+#             # put parenthesis around each record string
+#             values += "(" + ', '.join( values_list ) + "),\n"
+#         # remove the last comma and end SQL with a semicolon
+#         values = values[:-2] + ";"
+
+#         # concatenate the SQL string
+#         table_name = "company"
+#         sql_command = "INSERT INTO %s (%s)\nVALUES %s" % (
+#             table_name,
+#             ', '.join(columns),
+#             values
+#         )
+
+#         # insert values stored in sql_command
+#         cur.execute( sql_command )
+#         # commit the changes
+#         conn.commit()
+
+#     except (Exception, Error) as error:
+#             print("\nexecute_sql() error:", error)
+#     finally:
+#         if conn is not None:
+#             conn.close()
+
+# storeJsonToDB()
+
+storeCompaniesDataToJson()
