701c33a6cb848a86d74f9f6b069536e570e7f5f6
Alifia Rahmah <alifiarahmah03@gmail.com>
2022-06-27 12:46:49 +0700

    [UPDATE] Change cards response to JSON format

--- Data Scraping/src/api/main.py
+++ Data Scraping/src/api/main.py
@@ -16,39 +16,82 @@ def get_db():
 
 app = FastAPI()
 
-@app.get("/")
-def get_root():
-	return {"message": "Hello World"}
-
-@app.get("/api")
+@app.get("/api", status_code=200)
 def read_root():
 	return {"message": "Hello World"}
 
 @app.get("/api/cards")
 def read_cards():
 	# TODO get parameters from request, make case for searching by name, etc.
-	# TODO make response in JSON format
 	# connect to db
 	conn = get_db()
 	cur = conn.cursor()
 	# get all cards
 	cur.execute("SELECT * FROM cards")
 	cards = cur.fetchall()
+	# make JSON response
+	cardlist = []
+	for card in cards:
+		card = {
+			"id": card[0],
+			"name": card[1],
+			"rarity": card[2],
+			"character": card[3],
+			"attribute": card[4],
+			"max_power": card[5],
+			"max_influence": card[6],
+			"max_defense": card[7],
+		}
+		# search skills of the card
+		cur.execute("SELECT * FROM card_skills WHERE card_id = %s", (card["id"],))
+		skills = cur.fetchall()
+		# add skills to response
+		card["skills"] = []
+		for skill in skills:
+			cur.execute("SELECT * FROM skills WHERE skill_id = %s", (skill[1],))
+			skill_data = cur.fetchone()
+			card["skills"].append({
+				"type": skill_data[1],
+				"name": skill_data[2],
+			})
+		cardlist.append(card)
 	# close connection
 	cur.close()
 	conn.close()
 	# return response
-	return cards
+	return cardlist
 
 @app.get("/api/cards/{id}")
 def read_card(id: int):
-	# TODO make response in JSON format
 	# connect to db
 	conn = get_db()
 	cur = conn.cursor()
 	# get card by id
 	cur.execute("SELECT * FROM cards WHERE card_id = %s", (id,))
 	card = cur.fetchone()
+	# make response in JSON format
+	card = {
+		"id": card[0],
+		"name": card[1],
+		"rarity": card[2],
+		"character": card[3],
+		"attribute": card[4],
+		"max_power": card[5],
+		"max_influence": card[6],
+		"max_defense": card[7],
+	}
+	# search skills of the card
+	cur.execute("SELECT * FROM card_skills WHERE card_id = %s", (card["id"],))
+	skills = cur.fetchall()
+	# add skills to response
+	card["skills"] = []
+	for skill in skills:
+		cur.execute("SELECT * FROM skills WHERE skill_id = %s", (skill[1],))
+		skill_data = cur.fetchone()
+		card["skills"].append({
+			"type": skill_data[1],
+			"name": skill_data[2],
+		})
 	# close db connection
 	cur.close()
 	conn.close()
