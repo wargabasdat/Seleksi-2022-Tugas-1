ba443460ab1fea0c02182a0de17194ef8669d63d
Muhamad Fikri Nurohman <93314435+fikfikriii@users.noreply.github.com>
2022-07-01 06:09:29 +0700

    Delete index.js

--- Data Scraping/src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => res.send("Hello apa kabar"));
-
-app.get("/makan", (req, res) => res.send("uda makan"));
-
-app.listen(port, () => console.log("aplikasi berjalan"));
